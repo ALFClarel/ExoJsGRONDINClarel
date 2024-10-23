af573c5f8c9f493c18b8e8625dfa4a2da89d74b5
GRONDIN Clarel <clarelgrondin@gmail.com>
2024-10-23 11:31:55 +0400

    bonus

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function fonction(){
     document.getElementById('btn').innerHTML = document.getElementById('TextArea').value
     document.getElementById('TextArea').value = null
+
 };
+document.getElementById('TextArea').addEventListener('input', function(){
+document.getElementById('btn').innerHTML = document.getElementById('TextArea').value
+})
+    
+
